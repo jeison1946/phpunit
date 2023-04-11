1b4a06f2c3daf44cb53ca3120b640de36c6fd488
Json Jimenez <jeison@jeison.com>
2023-04-11 13:03:09 -0500

    initial test

--- modules/custom/phpunit_test/src/Controller/ControllerBasic.php
+++ modules/custom/phpunit_test/src/Controller/ControllerBasic.php
@@ -2,18 +2,34 @@
 namespace Drupal\phpunit_test\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Messenger\MessengerInterface;
+use Drupal\phpunit_test\Services\ConsultDate;
 
 class ControllerBasic extends ControllerBase{
 
-  public function view() {
-    $service = \Drupal::service('phpunit_test.service');
+  protected $phpunitService;
+  protected $messenger;
+
+  public function __construct(ConsultDate $consultDate, MessengerInterface $messenger)
+  {
+    $this->phpunitService = $consultDate;
+    $this->messenger = $messenger;
+  }
+
+  public function view($date) {
+    $service = $this->phpunitService;
     $currentDate = $service->getCurrentDate('custom', 'Y-m-d H:i:s');
     $custom = $service->getCurrentDate(strtotime('2021-05-03 16:20:22'), 'custom', 'Y-m-d H:i:s');
 
-    \Drupal::messenger()->addMessage(t('Cambiando Formato de fechas'));
+    $this->messenger->addMessage('Cambiando Formato de fechas');
+
+    if ($date == 1) {
+      $currentDate = time();
+    }
+
     return [
       '#type' => 'markup',
-      '#markup' => t('Fecha actual @current, fecha personalizada @custom', ['@current' => $currentDate, '@custom' => $custom]),
+      '#markup' => 'success'
     ];
   }
 }
